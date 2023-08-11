472098d770b4b9983c9d85c86f180f109653a7f6
dv1651st <str15012@byui.edu>
2023-08-11 06:41:26 -0700

    Coloring implementation

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,55 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let activeEditor = vscode.window.activeTextEditor;
+  if (activeEditor) {
+    updateDecorations(); // Run on activation
+  }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "css-variable-hsl-bulletproof-colorizer" is now active!');
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+    activeEditor = editor;
+    if (editor) {
+      updateDecorations();
+    }
+  }, null, context.subscriptions);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('css-variable-hsl-bulletproof-colorizer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CSS Variable HSL Bulletproof Colorizer!');
-	});
+  vscode.workspace.onDidChangeTextDocument(event => {
+    if (activeEditor && event.document === activeEditor.document) {
+      updateDecorations();
+    }
+  }, null, context.subscriptions);
 
-	context.subscriptions.push(disposable);
+  function updateDecorations() {
+    if (!activeEditor) {
+      return;
+    }
+
+    const text = activeEditor.document.getText();
+    const pattern = /--[a-zA-Z0-9-]+:\s*(\d{1,3}(?:\.\d{1,3})?(?:deg)?\s+\d{1,3}(?:\.\d{1,3})?%\s+\d{1,3}(?:\.\d{1,3})?%)(?=\s*;?)/g;
+    let match;
+
+    while ((match = pattern.exec(text))) {
+      const startPos = activeEditor.document.positionAt(match.index);
+      const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+      const hslMatch = match[1].split(' ');
+
+      // Extracting HSL values
+      const hue = parseFloat(hslMatch[0]);
+      const saturation = parseFloat(hslMatch[1]);
+      const lightness = parseFloat(hslMatch[2]);
+
+      // Converting HSL to CSS format
+      const hslColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+
+      // Determine text color based on lightness
+      const textColor = lightness > 50 ? 'black' : 'white';
+
+      const decorationType = vscode.window.createTextEditorDecorationType({
+        backgroundColor: hslColor,
+        color: textColor,
+      });
+      const decoration = { range: new vscode.Range(startPos, endPos) };
+      activeEditor.setDecorations(decorationType, [decoration]);
+    }
+  }
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
